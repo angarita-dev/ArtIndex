2f7e8b15534ab2445e0fc157931dbb2203b80890
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-26 17:39:15 -0500

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h1>Images & metadata by <span className="bold">Rijksmuseum</span></h1>
+    </div>
+  );
+}
+
+export default Footer;
