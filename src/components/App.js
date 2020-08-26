1c0905884454f026624174c555ecd619ba59d665
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-26 09:12:45 -0500

    Added App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Components
+import NavBar from './NavBar';
+import ResultsList from '../containers/ResultsList';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ResultsList />
+    </div>
+  );
+}
+
+export default App;
