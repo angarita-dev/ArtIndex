3eab03ca96f1ae9628cf5a5ba2ba47d2cc25eaee
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-27 10:37:00 -0500

    Added query input actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,20 @@
 // Action types
 export const CHANGE_FILTER = 'CHANGE_FILTER';
+export const CHANGE_QUERY_INPUT = 'CHANGE_QUERY_INPUT';
+export const DELETE_QUERY_INPUT = 'DELETE_QUERY_INPUT';
 
 // Actions
 export const changeFilter = filter => ({
   type: CHANGE_FILTER,
   filter,
+});
+
+export const changeQueryInput = input => ({
+  type: CHANGE_QUERY_INPUT,
+  input,
+});
+
+export const deleteQueryInput = () => ({
+  type: DELETE_QUERY_INPUT,
+  input: '',
 });
