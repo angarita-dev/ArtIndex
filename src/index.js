fa23ee74720ce07aba74aabd1f6d191797779507
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-27 10:00:47 -0500

    Initialized store with provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 // Style
 import './style/main.scss';
@@ -7,9 +9,17 @@ import './style/main.scss';
 // Components
 import App from './components/App';
 
+// Reducers
+import reducer, { DEFAULT_STATE } from './actions/reducers/index';
+
+const store = createStore(reducer, DEFAULT_STATE, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
