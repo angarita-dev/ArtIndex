348059c76f04f14dd5df97eabf8547d8a1633af5
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-28 11:10:26 -0500

    Moved app to containers folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 // Components
 import NavBar from './NavBar';
-import ResultsList from '../containers/ResultsList';
+import ResultsList from './ResultsList';
+import Footer from '../components/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ResultsList />
-      <div style={{height:'80px', backgroundColor: 'red'}} />
+      <Footer />
     </div>
   );
 }
