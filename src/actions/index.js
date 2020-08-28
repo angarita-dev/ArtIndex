0aaa4a31fed010f4b42e4550f318f39beba8648c
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-28 09:03:04 -0500

    Added generalQuery action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,24 @@
 // Action types
+// Filter
 export const CHANGE_FILTER = 'CHANGE_FILTER';
+
+// Query Input
 export const CHANGE_QUERY_INPUT = 'CHANGE_QUERY_INPUT';
 export const DELETE_QUERY_INPUT = 'DELETE_QUERY_INPUT';
 
+// Fetch query
+export const GENERAL_QUERY_PENDING = 'GENERAL_QUERY_PENDING';
+export const GENERAL_QUERY_SUCCESS = 'GENERAL_QUERY_SUCCESS';
+export const GENERAL_QUERY_ERROR = 'GENERAL_QUERY_ERROR';
+
 // Actions
+// filter
 export const changeFilter = filter => ({
   type: CHANGE_FILTER,
   filter,
 });
 
+// Query Input
 export const changeQueryInput = input => ({
   type: CHANGE_QUERY_INPUT,
   input,
@@ -17,4 +27,37 @@ export const changeQueryInput = input => ({
 export const deleteQueryInput = () => ({
   type: DELETE_QUERY_INPUT,
   input: '',
+});
+
+// Fetch Query
+export const generalQueryPending = () => ({
+  type: GENERAL_QUERY_PENDING,
+});
+
+export const generalQuerySuccess = results => {
+  const imgUrl = (result) => {
+    return result.hasImage && result.showImage && result.permitDownload ?
+      result.webImage.url :
+      '';
+  };
+
+  const parsedData = results.artObjects.map(result => {
+    return {
+      id: result.id,
+      title: result.title,
+      maker: result.principalOrFirstMaker,
+      longTitle: result.longTitle,
+      imgUrl: imgUrl(result),
+    }
+  });
+
+  return {
+    type: GENERAL_QUERY_SUCCESS,
+    generalQueryResults: parsedData,
+  }
+};
+
+export const generalQueryError = error => ({
+  type: GENERAL_QUERY_ERROR,
+  error: error,
 });
